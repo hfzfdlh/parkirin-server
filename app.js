ad1e605c55117f4e046da11f3ee64e588dca8568
hfzfdlh <fadillahhafiz.fadillah@gmail.com>
2023-08-14 12:39:03 +0700

    chore: initial setup

---
+++ app.js
@@ -0,0 +1,13 @@
+require('dotenv').config()
+
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+app.use('/',require('./routes/router'))
+
+app.listen(port, ()=>{
+    console.log(`listening ${port}`);
+})
